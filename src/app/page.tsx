688d89438881be93c13c1ef3b050c9540a7fc54d
Valerii_Panov <val250640@mail.ru>
2025-12-21 09:53:24 +1000

    добавил глобальные цвета

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import styles from "./page.module.css";
 
+/** Главная страница сайта */
 export default function Home() {
   return (
     <div className={styles.page}>
-
+        <h1>Наши ценности</h1>
     </div>
   );
 }
