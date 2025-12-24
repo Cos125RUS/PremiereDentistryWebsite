b2231f5d6eff6572cc7392378ef2249bfa5d3bff
Valerii_Panov <val250640@mail.ru>
2025-12-24 18:15:54 +1000

    сверстал карточки новостей

--- src/app/components/pages/index/components/NewsItem.tsx
+++ src/app/components/pages/index/components/NewsItem.tsx
@@ -14,7 +14,7 @@ export const NewsItem: React.FC<Props> = ({news}) => {
                 <Image width={399} height={470} src={news.img} alt={news.title}/>
                 <button>Новость</button>
             </div>
-            <div>
+            <div className={styles.text}>
                 <p>{news.date}</p>
                 <h6>{news.title}</h6>
                 <p>{news.description}</p>
