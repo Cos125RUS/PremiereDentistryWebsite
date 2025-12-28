472c78cbb05236bf28e72c50b2e3c066076118ff
Valerii_Panov <val250640@mail.ru>
2025-12-28 20:42:48 +1000

    доделал слайдер мобильной версии

--- src/app/components/pages/index/components/Values.tsx
+++ src/app/components/pages/index/components/Values.tsx
@@ -4,11 +4,54 @@ import {valuesData} from "@/app/components/pages/index/data/values.data";
 import styles from "./Values.module.scss";
 import Image from "next/image";
 import classNames from "classnames";
-import {ValuesSlider} from "@/app/components/pages/index/components/ValuesSlider";
+import {useEffect, useRef, useState} from "react";
 
 /** Блок ценностей */
 export const Values = () => {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const elementWidth = useRef(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [touchStartPosition, setTouchStartPosition] = useState(0);
+    const [touchOffset, setTouchOffset] = useState(0);
     const components: React.ReactNode[] = [];
+
+    /** Определение ширины перелистываемого элемента */
+    useEffect(() => {
+        if (window.innerWidth < 481 && containerRef.current) {
+            elementWidth.current = containerRef.current.children?.[0]?.clientWidth;
+        }
+    }, []);
+
+    /** Перелистывание свайпом */
+    const swipe = (index: number) => {
+        setCurrentIndex(index);
+        if (containerRef.current) {
+            containerRef.current.scrollLeft = elementWidth.current * index;
+        }
+    };
+
+    /** Отслеживание касания */
+    const touchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+        setTouchStartPosition(e.touches[0].clientX);
+    };
+
+    /** Отслеживание перемещения пальца */
+    const touchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+        setTouchOffset(e.touches[0].clientX - touchStartPosition);
+    };
+
+    /** Отпускание пальца */
+    const touchStop = () => {
+        const swipeLength = elementWidth.current / touchOffset;
+        //Сдвиг влево
+        if (swipeLength > 0 && swipeLength < 5 && currentIndex > 0) {
+            swipe(currentIndex - 1);
+        }
+        //Сдвиг вправо
+        if (swipeLength < 0 && swipeLength > -5 && currentIndex < components.length - 1) {
+            swipe(currentIndex + 1);
+        }
+    };
 
     for (let i = 0; i < valuesData.length; i = i + 2) {
         const numberLeft = i < 9 ? `0${i + 1}` : i + 1;
@@ -39,12 +82,20 @@ export const Values = () => {
         </div>);
     }
 
-    return <div className={styles.container}>
-        {components.map((component, index) => (
-            <div key={`value-${index}`} className={styles.row}>
-                {component}
-            </div>
-        ))}
-        <ValuesSlider length={Math.ceil(components.length)}/>
-    </div>
+    return <>
+        <div className={styles.container} ref={containerRef}
+             onTouchStart={touchStart} onTouchMove={touchMove} onTouchEnd={touchStop}>
+            {components.map((component, index) => (
+                <div key={`value-${index}`} className={styles.row}>
+                    {component}
+                </div>
+            ))}
+        </div>
+        <ul className={styles.slider}>
+            {Array(components.length).fill(false).map((_, i) => (
+                <li key={`slider-${i}`}
+                    className={classNames(styles.item, i === currentIndex && styles.item__active)}></li>
+            ))}
+        </ul>
+    </>
 };
