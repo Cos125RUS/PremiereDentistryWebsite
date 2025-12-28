bfdb97f4d7be4c2f8d072843784dfd581c8df8ed
Valerii_Panov <val250640@mail.ru>
2025-12-28 12:38:29 +1000

    доделал планшетную вёрстку

--- src/app/components/pages/index/components/NewsBox.tsx
+++ src/app/components/pages/index/components/NewsBox.tsx
@@ -1,20 +1,30 @@
+"use client";
+
 import {CheckSvg} from "@/app/components/ui/icon/CheckSvg";
 import styles from "./News.module.scss";
 import {RootState, useAppSelector} from "@/app/utils/storage/store";
 import classNames from "classnames";
 import {NewsItem} from "@/app/components/pages/index/components/NewsItem";
 import Link from "next/link";
-import {useCallback, useMemo, useRef, useState} from "react";
+import {useCallback, useEffect, useMemo, useRef, useState} from "react";
 
 //Ширина карточки новостей
-const NEWS_CARD_WIDTH = 407;
+const NEWS_CARD_WIDTH = 399;
 
 /** Блок новостей */
 export const NewsBox = () => {
     const {news, newsCount} = useAppSelector((state: RootState) => state.site);
     const listRef = useRef<HTMLUListElement | null>(null);
     const [offset, setOffset] = useState(0);
+    const cardWidth = useRef(NEWS_CARD_WIDTH);
     const offsetLength = useMemo(() => news.length - 4, [news.length]);
+
+    /** Определение единицы смещения */
+    useEffect(() => {
+        if (news.length && listRef.current) {
+            cardWidth.current = listRef.current.children?.[0]?.clientWidth ?? NEWS_CARD_WIDTH;
+        }
+    }, [news.length]);
 
     /** Прокрутка новостей */
     const scrollNews = useCallback((arg: number) => {
@@ -26,7 +36,7 @@ export const NewsBox = () => {
                 newOffset = offsetLength;
             }
 
-            listRef.current.scrollLeft = NEWS_CARD_WIDTH * newOffset;
+            listRef.current.scrollLeft = (cardWidth.current + 8) * newOffset;
             setOffset(newOffset);
         }
     }, [offset, offsetLength, listRef]);
--- src/app/components/pages/index/components/Values.tsx
+++ src/app/components/pages/index/components/Values.tsx
@@ -12,22 +12,26 @@ export const Values = () => {
     for (let i = 0; i < valuesData.length; i = i + 2) {
         const numberLeft = i < 9 ? `0${i + 1}` : i + 1;
         const numberRight = i < 10 ? `0${i + 2}` : i + 2;
-        const leftComponent = <div>
-            <p>{numberLeft}</p>
-            <div className={styles.textContent}>
-                <h6 className={styles.label}>{valuesData[i].label}</h6>
-                <p className={styles.paragraph}>{valuesData[i].text}</p>
+        const leftComponent = <div className={styles.card}>
+            <div>
+                <p>{numberLeft}</p>
+                <div className={styles.textContent}>
+                    <h6 className={styles.label}>{valuesData[i].label}</h6>
+                    <p className={styles.paragraph}>{valuesData[i].text}</p>
+                </div>
             </div>
             <Image width="274" height="278" src={valuesData[i].img} alt={valuesData[i].alt}/>
         </div>;
 
         components.push(<div key={`value-${i}`} className={classNames(styles.box, "max-width")}>
             {leftComponent}
-            {!!valuesData[i + 1] && <div>
-                <p>{numberRight}</p>
-                <div className={styles.textContent}>
-                    <h6 className={styles.label}>{valuesData[i + 1].label}</h6>
-                    <p className={styles.paragraph}>{valuesData[i + 1].text}</p>
+            {!!valuesData[i + 1] && <div className={styles.card}>
+                <div>
+                    <p>{numberRight}</p>
+                    <div className={styles.textContent}>
+                        <h6 className={styles.label}>{valuesData[i + 1].label}</h6>
+                        <p className={styles.paragraph}>{valuesData[i + 1].text}</p>
+                    </div>
                 </div>
                 <Image width="274" height="278" src={valuesData[i + 1].img} alt={valuesData[i + 1].alt}/>
             </div>}
